d141ad8b05a664b4d803a548d0b40d764c7d1fa1
sheida noori <sheida.nouri@gmail.com>
2023-03-27 16:21:45 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+function findMatching(names,str){
+    return names.filter(name => name.toLowerCase() === str.toLowerCase())
+    }
+    
+    function fuzzyMatch(names,str){
+    return names.filter(name=>name.search(str)== 0 )
+    
+    }
+    
+    function matchName(drivers,str){
+        const matchName = drivers.filter(obj => obj.name.toLowerCase() === str.toLowerCase())
+    return matchName
+    }
